f2edf7b4348137d98b9d5047830d72ea8c8aae89
Joseph Belmonte <joseph.belmonte@gmail.com>
2021-10-21 17:17:00 -0400

    added role based masking

--- chooseyourvisitor.js
+++ chooseyourvisitor.js
@@ -71,12 +71,52 @@ function showLoader() {
   document.getElementById("group-id").innerHTML = document.getElementById(
     "groupId"
   ).value;
+  document.querySelector("#allowed-roles-list").innerText = document.querySelector("#allowed-roles").value
+  document.querySelector("#environment-chosen").innerText = document.getElementById("environment").value;
 }
 
 function sessionStarted() {
   console.log("the session has started");
   document.getElementById("loader").style.display = "none";
   document.getElementById("status-message").innerHTML = "";
+}
+
+function getAllowedRoles() {
+  var allowedRoles = document.querySelector("#allowed-roles").value
+  var allowedRolesArray = allowedRoles.split(',')
+  var allowedRolesArrayTrimmed = allowedRolesArray.map(role => role.trim())
+  return allowedRolesArrayTrimmed
+}
+
+function shouldPageBeMasked(params) {
+  var allowedRolesList = getAllowedRoles()
+  var agentlist = params.agents.agentlist
+  // If the Allowed Role input was left blank, allowedRolesList will be an array of length 1 with value of ''.
+  // We don't want to mask the page in this situation.
+  if (allowedRolesList.length === 1 && allowedRolesList[0] === "") {
+    return false
+  }
+  // The logic here is that every agent in the session must be in a role that's included in the allowed list
+  // If even 1 agent is in a role not on the allowed list, then the page is masked for all agents
+  return !agentlist.every(agent => allowedRolesList.includes(agent.agentrole))
+}
+
+// Hook function called by cobrowse script to get masking information
+var GLANCE_COBROWSE = {}
+GLANCE_COBROWSE.getMaskingDescriptor = async function (params) {
+    return new Promise(async (resolve, reject) => {
+
+        let maskingdescriptor = {};
+
+        // If agent information is not yet available, mask everything with a data-agentrole attribute
+        if (!params.agents || params.agents.count === 0)
+          maskingdescriptor.maskpage = false
+        else (
+          maskingdescriptor.maskpage = shouldPageBeMasked(params)
+        )
+        console.log('maskingdescriptor is ', maskingdescriptor)
+        resolve(maskingdescriptor);
+    })
 }
 
 function submitClicked() {
@@ -86,6 +126,7 @@ function submitClicked() {
   url.searchParams.set('environment', document.getElementById("environment").value);
   url.searchParams.set('visitorId', document.getElementById("visitorId").value);
   url.searchParams.set('beta', document.querySelector("#use-beta").checked);
+  url.searchParams.set('allowedroles', encodeURI(document.querySelector("#allowed-roles").value));
   window.history.pushState({}, '', url)
   addCobrowseScript();
   hideUserInput();
@@ -104,6 +145,7 @@ window.onload = event => {
     const environment = urlParams.get('environment');
     const visitorId = urlParams.get('visitorId');
     const beta = urlParams.get('beta');
+    const allowedroles = urlParams.get('allowedroles');
     if (groupid){
       document.getElementById("groupId").value = groupid
     }
@@ -115,5 +157,8 @@ window.onload = event => {
     }
     if (beta == "true") {
       document.querySelector("#use-beta").checked = true
+    }
+    if (allowedroles) {
+      document.querySelector("#allowed-roles").value = decodeURI(allowedroles)
     }
 };
